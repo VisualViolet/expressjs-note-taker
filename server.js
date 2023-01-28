8e353337148dbdaf88453a37a71ade543942af9c
VisualViolet <savannah_b_miller@outlook.com>
2023-01-27 19:55:05 -0800

    added required packages and middleware

---
+++ server.js
@@ -0,0 +1,16 @@
+// Required packages
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+const PORT = 3001;
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+    console.log(`Listening at http://localhost:${PORT}`);
+})
