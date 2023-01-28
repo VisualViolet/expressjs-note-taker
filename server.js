cef487af559a6a35d322e48a0beced3d1dad8adf
VisualViolet <savannah_b_miller@outlook.com>
2023-01-27 20:00:06 -0800

    added HTML routes

--- server.js
+++ server.js
@@ -11,6 +11,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+// HTML Routes
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+})
+
+app.get('/notes', (req,res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+})
+
+
+// Server listener
 app.listen(PORT, () => {
     console.log(`Listening at http://localhost:${PORT}`);
 })
