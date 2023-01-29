fd1d31c404ec202a61e91fc23bdc4f96e4c88eb8
VisualViolet <savannah_b_miller@outlook.com>
2023-01-29 11:18:17 -0800

    added unique ID to notes

--- server.js
+++ server.js
@@ -1,64 +1,58 @@
 // Required packages
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
-const notes = require('./db/db.json');
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const Notes = require("./db/db.json");
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 const PORT = 3001;
 
-
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // HTML and API Routes
-app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
-app.get('/notes', (req,res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-})
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
 
-app.get('/api/notes', (req,res) => {
-    res.json(notes);
-})
+app.get("/api/notes", (req, res) => {
+  res.json(Notes);
+});
 
-app.get('*', (req,res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-})
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
 // POST route to save new note to db.json
 app.post("/api/notes", (req, res) => {
-    fs.readFile("./db/db.json", "utf-8", (err, data) => {
-      if (err) 
-      {
-        res.status(500).send("Server Error. Please try again.");
-        throw err;
-      } 
-      else 
-      {
-        const noteData = JSON.parse(data);
-        noteData.push(req.body);
-        fs.writeFile("./db/db.json", JSON.stringify(noteData, null, 2), (err) => {
-          if (err) 
-          {
-            res.status(500).send("Server error. Please try again.");
-            throw err;
-          } 
-          else 
-          {
-            res.send("Success! Note added!");
-          }
-        });
-      }
-    });
+  fs.readFile("./db/db.json", "utf-8", (err, data) => {
+    if (err) {
+      res.status(500).send("Oh no! There was a server error.");
+      throw err;
+    } else {
+      const notes = JSON.parse(data);
+      const newNote = { title: req.body.title, text: req.body.text, noteId: uuidv4() };
+      notes.push(newNote);
+      fs.writeFile("./db/db.json", JSON.stringify(notes, null, 4), (err) => {
+        if (err) {
+          res.status(500).send("Oh no! There was a server error.");
+          throw err;
+        } else {
+          res.send("Success! New note added!");
+        }
+      });
+    }
   });
-  
+});
 
 // Server listener
 app.listen(PORT, () => {
-    console.log(`Listening at http://localhost:${PORT}`);
-})
+  console.log(`Listening at http://localhost:${PORT}`);
+});
